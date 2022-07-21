563a3ba74dc2fea0762c48ba31cd34e05a660c61
Erick Villafuerte <erick.m.villa.r@gmail.com>
2022-07-21 12:00:18 -0500

    pagina principal de estudiantes terminada

--- src/pages/juegos/index.vue
+++ src/pages/juegos/index.vue
@@ -1,3 +1,55 @@
 <template>
-  <h1>JUEGOS</h1>
+  <h1>BIENVENIDO A LA SALA DE JUEGOS Y RECOMPENSAS</h1>
+  <section>
+    <div class="w-100 row">
+        <div href="" class="clases col text-dark container w-25 border border-dark rounded d-flex flex-column m-3">
+          <div>
+            <img class="img w-100 img-fluid" src="../../assets/imgs/biologia.jpg" alt="portada biologia">
+            <p>Nivel Necesario: <span class="text-weigth-bold">5</span> </p>
+            <h4 class="class_one h-100 border-top border-dark">Juego de cartas</h4>
+          </div>
+          <a href="">Jugar</a>
+        </div>
+        <div href="" class="clases col text-dark container w-25 border border-dark rounded d-flex flex-column m-3">
+          <div>
+            <img class="img w-100 img-fluid" src="../../assets/imgs/biologia.jpg" alt="portada biologia">
+            <p>Nivel Necesario: <span class="text-weigth-bold">10</span> </p>
+            <h4 class="class_one h-100 border-top border-dark">Juego Memorama</h4>
+          </div>
+          <a href="">Jugar</a>
+        </div>
+        <div href="" class="clases col text-dark container w-25 border border-dark rounded d-flex flex-column m-3">
+          <div>
+            <img class="img w-100 img-fluid" src="../../assets/imgs/biologia.jpg" alt="portada biologia">
+            <p>Nivel Necesario: <span class="text-weigth-bold">15</span> </p>
+            <h4 class="class_one h-100 py-4 border-top border-dark">Juego Pokemon</h4>
+          </div>
+          <a href="">Jugar</a>
+        </div>
+        <div href="" class="clases col text-dark container w-25 border border-dark rounded d-flex flex-column m-3">
+          <div>
+            <img class="img w-100 img-fluid" src="../../assets/imgs/biologia.jpg" alt="portada biologia">
+            <p>Nivel Necesario: <span class="text-weigth-bold">20</span> </p>
+            <h4 class="class_one h-100 py-4 border-top border-dark">Juego Snake</h4>
+          </div>
+          <a href="">Jugar</a>
+        </div>
+        <div href="" class="clases col text-dark container w-25 border border-dark rounded d-flex flex-column m-3">
+          <div>
+            <img class="img w-100 img-fluid" src="../../assets/imgs/biologia.jpg" alt="portada biologia">
+            <p>Nivel Necesario: <span class="text-weigth-bold">25</span> </p>
+            <h4 class="class_one h-100 py-4 border-top border-dark">Tajeta de regalo</h4>
+          </div>
+          <a href="">Obtener Premio</a>
+        </div>
+        <div href="" class="clases col text-dark container w-25 border border-dark rounded d-flex flex-column m-3">
+          <div>
+            <img class="img w-100 img-fluid" src="../../assets/imgs/biologia.jpg" alt="portada biologia">
+            <p>Nivel Necesario: <span class="text-weigth-bold">30</span> </p>
+            <h4 class="class_one h-100 py-4 border-top border-dark">Juego Naves</h4>
+          </div>
+          <a href="">Jugar</a>
+        </div>
+    </div>
+  </section>
 </template>
