eec405198f3bc32daffa8812829788c9668f310d
devleon <devleon00@gmail.com>
2022-07-22 16:49:32 -0500

    Agregando pages estudiante

---
+++ src/pages/estudiantes/cursos.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <h3>Bienvenido <span class="bienvenida">{{name}}</span></h3>
+    <nav class="navbar px-4">
+      <li><a href="./comunidad" class="comunidad">Comunidad</a></li>
+      <li><a href="" class="agenda">Mi agenda</a> </li>
+      <li class="neon"><a href="/juegos"><img class="icon-game" src="/icons/play.png" alt=""> JUEGOS</a></li>
+      <li><a href="" class="progreso">Mi Progreso</a> </li>
+      <li><a href="" class="cursos">Mis Cursos</a> </li>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
