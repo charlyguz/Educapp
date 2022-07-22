1a063bda033240ada254ece60b2d10665287ee18
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-07-22 00:01:28 -0500

    Format mentorship date

--- src/components/tutor/dashboard/LiveSessionsTable.vue
+++ src/components/tutor/dashboard/LiveSessionsTable.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { MockLiveSession } from '~/util/types/mocking/mock-live-session'
+import { SPANISH_MESSAGES } from '~/util/constants/time-ago'
 
 const router = useRouter()
 
@@ -7,6 +8,14 @@ const props = defineProps<{
   headers: string[]
   liveSessions: MockLiveSession[]
 }>()
+
+function formatDate(date: Date) {
+  const formattedDate = useDateFormat(date, 'DD-MM-YY').value
+  const formatTime = useDateFormat(date, 'HH:mm').value
+  return `${formattedDate} a las ${formatTime} (${useTimeAgo(date, {
+    messages: SPANISH_MESSAGES,
+  }).value.replaceAll('"', '')})`
+}
 
 function goMeeting(sessionId: string) {
   router.push(`/tutorias/${encodeURIComponent(sessionId)}`)
@@ -36,13 +45,19 @@ function goMeeting(sessionId: string) {
               }}</span>
             </td>
             <td>
-              <span class="bg-blight">{{ session.scheduledAt }}</span>
+              <span class="bg-blight">{{
+                formatDate(session.scheduledAt)
+              }}</span>
             </td>
             <td>
               <span class="bg-blight">{{ session.sessionId }}</span>
             </td>
             <td class="text-center">
-              <button type="button" class="btn btn-outline-primary" @click="goMeeting(session.sessionId)">
+              <button
+                type="button"
+                class="btn btn-outline-primary"
+                @click="goMeeting(session.sessionId)"
+              >
                 <span>Comenzar <i class="bi-arrow-right-short"></i></span>
               </button>
             </td>
