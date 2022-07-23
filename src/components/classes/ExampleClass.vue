79591ecf8e8c6adc2efab48d463f63b2b50e0a0c
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-07-23 10:52:33 -0500

    Add single class regla-de-tres

---
+++ src/components/classes/ExampleClass.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const showQuiz = ref(false)
+function toogleQuiz() {
+  showQuiz.value = !showQuiz
+}
+</script>
+<template>
+  <div class="container-xxl flex-wrap flex-md-nowrap">
+    <div class="row">
+      <!-- <div class="col">Column</div> -->
+      <div class="col">
+        <iframe
+          width="auto"
+          height="auto"
+          src="https://www.youtube.com/embed/_JeR__bXzG0"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+        <regla-de-tres></regla-de-tres>
+        <!-- <button type="button" class="btn btn-success" @click="toogleQuiz()">
+          Mostar / ocultar quiz
+        </button> -->
+        <quiz></quiz>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang=""></style>
---
+++ src/pages/estudiantes/regla-de-tres.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <example-class></example-class>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
