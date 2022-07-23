419e1d7da936db9ab581312c5ef15925472415a2
Carlos Aguilera <ckarlos_95@hotmail.es>
2022-07-22 22:11:58 -0500

    Add pwa config and images

--- vite.config.ts
+++ vite.config.ts
@@ -98,8 +98,8 @@ export default defineConfig({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
+        name: 'Educapp',
+        short_name: 'Educapp',
         theme_color: '#ffffff',
         icons: [
           {
