9d0444c2f48a6f8d5075a6f154e39d4070b7ddc2
devleon <devleon00@gmail.com>
2022-07-23 10:13:18 -0500

    Agrego notificaciones

--- src/pages/estudiantes/progreso.vue
+++ src/pages/estudiantes/progreso.vue
@@ -1,7 +1,5 @@
 <template>
   <navbar link1="/estudiantes/comunidad" name1="Comunidad" link2="/estudiantes/agenda" name2="Agenda" link3="./"  link4="/estudiantes/progreso" name4="Progreso" link5="/estudiantes/cursos" name5="Mis Cursos"></navbar>
-
-  
 </template>
 
 <script>
